4738f02366f69a624d0ceb5e3de6565d14da9532
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-10-29 19:13:24 -0700

    bug(random not working) see how to fix the bug with generating a random string for the computer

--- src/MainFile.kt
+++ src/MainFile.kt
@@ -8,7 +8,7 @@ import kotlin.random.Random
 // compare choices in while loop and end loop when winner variable is 5
 var computerCounter = 0;
 var userCounter = 0;
-var winner : Number = 0;
+var winner = 0;
 
 fun main() {
 
@@ -16,38 +16,54 @@ fun main() {
     val randomNumber =  Random.nextInt(0,2)
     println(randomNumber)
 
-//    println("please enter a value")
-//    val userValue = readLine();
 
     var computerChoice = generateStringValue(randomNumber)
-    val enteredString = readLine()
+    println("Please enter a value")
+    var enteredString = readLine()
+
+
+
+    while(winner <= 5) {
+        compareChoices(enteredString, generateStringValue(randomNumber))
+        println("Please enter a value")
+        enteredString = readLine()
+
+    }
+
 }
 
+
 // compare choices
-fun compareChoices(user: String, computer: String) {
+fun compareChoices(user: String?, computer: String) {
     if(user == computer) {
         println("it's a tied!")
     }else if(user == "paper" && computer == "rock") {
         userCounter++
+        winner++
         println("user choose $user, computer choose $computer")
     } else if(user == "rock" && computer == "scissors") {
         userCounter++
+        winner++
         println("user choose $user, computer choose $computer")
 
     } else if(user == "scissors" && computer == "paper") {
         userCounter++
+        winner++
         println("user choose $user, computer choose $computer")
 
     } else if (computer == "paper" && user == "rock") {
         computerCounter++
+        winner++
         println("computer choose $computer, user choose $user")
 
     } else if(computer == "rock" && user == "scissors") {
         computerCounter++
+        winner++
         println("computer choose $computer, user choose $user")
 
     } else if(computer == "scissors" && user == "paper") {
         computerCounter++
+        winner++
         println("computer choose $computer, user choose $user")
 
     }
